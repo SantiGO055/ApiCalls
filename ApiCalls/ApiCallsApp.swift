dc360b38c8525e6facb2062b925ac50355c590ce
SantiGO055 <santigonzalez05@gmail.com>
2024-01-07 21:35:44 -0300

    Initial Commit

---
+++ ApiCalls/ApiCallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiCallsApp.swift
+//  ApiCalls
+//
+//  Created by Santiago Gonzalez on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ApiCallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
